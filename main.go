569c3a621db4905dc6e0329de878abf1c25fe07c
Ryan <wm.ryan.mooney@gmail.com>
2025-03-05 14:01:05 -0900

    added a sign to be sure the canvas it painting properly

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "fyne.io/fyne/v2/app"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"image/color"
+)
 
 func main() {
 	a := app.New()
-
 	w := a.NewWindow("Hello")
+
+	w.SetContent(makeSign())
+	w.SetPadded(false)
+	w.Resize(fyne.NewSize(120, 120))
 	w.ShowAndRun()
+}
+
+func makeSign() fyne.CanvasObject {
+	bg := canvas.NewCircle(color.NRGBA{255, 0, 0, 255})
+	bg.StrokeColor = color.White
+	bg.StrokeWidth = 5
+
+	bar := canvas.NewRectangle(color.White)
+
+	// manual layouts, as used here, do not automatically resize.
+	c := container.NewWithoutLayout(bg, bar)
+
+	bg.Resize(fyne.NewSize(100, 100))
+	bg.Move(fyne.NewPos(10, 10))
+	bar.Resize(fyne.NewSize(80, 20))
+	bar.Move(fyne.NewPos(20, 50))
+
+	return c
 }
